1382f008e39572f2518d5f92f33dde1e91841b95
maubayuelo <maubayuelo@gmail.com>
2025-10-04 12:19:14 -0400

    Fix: Link to the web app manifest (served from public/assets/favicon/site.webmanifest)

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      {/* Link to the web app manifest (served from public/assets/favicon/site.webmanifest) */}
+      <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+    </>
+  );
+}
